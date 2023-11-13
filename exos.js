b5d0b12ac4fee185121f5ddddd6b4625d4eaa69a
Augustin <raygister@gmail.com>
2023-11-13 11:24:40 +0100

    Exo 2

--- exos.js
+++ exos.js
@@ -8,3 +8,16 @@ function myMap(arr, func) {
 
     return newArr;
 }
+
+//Exo 2
+function myFilter(arr, func) {
+    var filteredArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (func(arr[i], i, arr)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+  
+    return filteredArr;
+}

4860ade73d681a447d51af43bfe52d41ece5ed88
Augustin <raygister@gmail.com>
2023-11-13 11:24:51 +0100

    Exo 3

--- exos.js
+++ exos.js
@@ -21,3 +21,14 @@ function myFilter(arr, func) {
   
     return filteredArr;
 }
+
+//Exo 3
+function myReduce(arr, func, initialValue) {
+    var accumulator = initialValue !== undefined ? initialValue : 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        accumulator = func(accumulator, arr[i], i, arr);
+    }
+    
+    return accumulator;
+}
